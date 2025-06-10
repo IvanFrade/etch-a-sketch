d2935860a055842e6ad93adb0f60e22ccc781081
IvanFrade <fradellin.ivan@gmail.com>
2025-06-10 23:47:10 +0200

    Add basic styling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let gameArea = document.querySelector("#drawing-area");
 
 for (let i = 0; i < 16; i++) {
     let div = document.createElement("div");
+    div.classList.add("drawing-square");
     div.textContent = i;
     gameArea.appendChild(div);
 }
