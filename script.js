6883a5e7d938278d3e214c292b79d0b43c6912a9
IvanFrade <fradellin.ivan@gmail.com>
2025-06-10 23:53:20 +0200

    Start adding size area button

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let gameArea = document.querySelector("#drawing-area");
-
-for (let i = 0; i < 16; i++) {
-    let div = document.createElement("div");
-    div.classList.add("drawing-square");
-    div.textContent = i;
-    gameArea.appendChild(div);
+function createDrawArea(squareNumber) {
+    for (let i = 0; i < squareNumber; i++) {
+        let div = document.createElement("div");
+        div.classList.add("drawing-square");
+        gameArea.appendChild(div);
+    }
 }
+
+let gameArea = document.querySelector("#drawing-area");
+document.querySelector("#area-size-btn").addEventListener(() => {
+    let squareNumber = 
+});
+
+createDrawArea(16);
