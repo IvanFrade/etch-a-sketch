152a1d13902da80cb88f0e4f22afeb8b65d9e156
IvanFrade <fradellin.ivan@gmail.com>
2025-06-11 13:57:02 +0200

    Fix grid squares scaling

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function createDrawArea(squareNumber) {
     for (let i = 0; i < squareNumber; i++) {
         let div = document.createElement("div");
         div.classList.add("drawing-square");
+        let size = 600 / Math.sqrt(squareNumber);
+        div.style = `width: ${size}px; height: ${size}px;`;
         squareArray.push(div);
     }
 
@@ -15,7 +17,7 @@ let gameArea = document.querySelector("#drawing-area");
 document.querySelector("#area-size-btn").addEventListener("click", () => {
     let squareNumber = prompt("Insert new square number");
     gameArea.child
-    createDrawArea(squareNumber);
+    createDrawArea(squareNumber * squareNumber);
 });
 
 createDrawArea(16);
