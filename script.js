c3317d307eb457860c488850e19c2461feef9f64
IvanFrade <fradellin.ivan@gmail.com>
2025-06-11 13:45:56 +0200

    Add resize button

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function createDrawArea(squareNumber) {
+    const squareArray = [];
+    
     for (let i = 0; i < squareNumber; i++) {
         let div = document.createElement("div");
         div.classList.add("drawing-square");
-        gameArea.appendChild(div);
+        squareArray.push(div);
     }
+
+    gameArea.replaceChildren(...squareArray);
 }
 
 let gameArea = document.querySelector("#drawing-area");
-document.querySelector("#area-size-btn").addEventListener(() => {
-    let squareNumber = 
+
+document.querySelector("#area-size-btn").addEventListener("click", () => {
+    let squareNumber = prompt("Insert new square number");
+    gameArea.child
+    createDrawArea(squareNumber);
 });
 
 createDrawArea(16);
