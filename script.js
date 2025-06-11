b94f372661c8175fe1f3fb523b0300a6289ed104
IvanFrade <fradellin.ivan@gmail.com>
2025-06-11 19:47:04 +0200

    Make trail permanent, add clear button
    Made the trail permanent on hover by adding
    a mouseenter event to the divs.
    Added a clear button to clear the drawing area.
    Changed starting area from 4x4 to 16x16

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function createDrawArea(squareNumber) {
+    squareNumber = squareNumber > 100 ? 1000 : (squareNumber * squareNumber);
     const squareArray = [];
     
     for (let i = 0; i < squareNumber; i++) {
         let div = document.createElement("div");
+
         div.classList.add("drawing-square");
-        let size = 600 / Math.sqrt(squareNumber);
+        let size = 600 / Math.sqrt(squareNumber);        
         div.style = `width: ${size}px; height: ${size}px;`;
+        div.addEventListener("mouseenter", () => { div.classList = "colored"});
+
         squareArray.push(div);
     }
 
@@ -13,11 +17,15 @@ function createDrawArea(squareNumber) {
 }
 
 let gameArea = document.querySelector("#drawing-area");
+let squareNumber = 16; // Default size is 16 * 16
 
 document.querySelector("#area-size-btn").addEventListener("click", () => {
-    let squareNumber = prompt("Insert new square number");
-    gameArea.child
-    createDrawArea(squareNumber * squareNumber);
+    squareNumber = prompt("Input the number of squares per side (max.100)");
+    createDrawArea(squareNumber);
 });
 
-createDrawArea(16);
+document.querySelector("#clear-btn").addEventListener("click", () => {
+    createDrawArea(squareNumber);
+});
+
+createDrawArea(squareNumber);
